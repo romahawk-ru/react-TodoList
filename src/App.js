7a2b87fdee5e439f209bd1b348a33f140a42699f
romahawk-ru <zotov_r_g@mail.ru>
2022-07-25 02:23:58 +0300

    # 1 create Todo List app

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import TodoList from './Todo/TodoList';
+import Context from './Todo/context';
+import AddTodo from './Todo/AddTodo';
+
+function App() {
+  const [todos, setTodos] = React.useState([
+    {id: 1, completed: false, title: 'Купить хлеб'},
+    {id: 2, completed: true, title: 'Купить масло'},
+    {id: 3, completed: false, title: 'Купить субару'},
+  ])
+
+
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    )
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(todos.concat([
+      {
+        title,
+        id: Date.now(),
+        completed: false
+      }
+    ]))
+  }
+
+  return (
+    <Context.Provider value={{ removeTodo }}>
+      <div className='wrapper'>
+        <h1>React tutorial</h1>
+
+        <AddTodo onCreate={addTodo} />
+
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggleTodo} />
+          ) : (
+            <p> No todos! </p>
+          )}
+        
+      </div>
+    </Context.Provider>
+
+  );
+}
+
+export default App;
